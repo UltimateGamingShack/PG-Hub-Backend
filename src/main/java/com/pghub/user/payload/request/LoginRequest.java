9fd5566aa5677c64ce01558466a9ca733eb47c27
UGS <ugsgamer@gmail.com>
2024-12-18 15:47:05 +0530

    Completed validation for LoginRequest DTO, added lombok getter setter for JwtResponse, MessageResponse,loginRequest, and added log4j2.properties file

--- src/main/java/com/pghub/user/payload/request/LoginRequest.java
+++ src/main/java/com/pghub/user/payload/request/LoginRequest.java
@@ -1,27 +1,20 @@
 package com.pghub.user.payload.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class LoginRequest {
-	@NotBlank
+	@NotBlank(message = "{username_invalid_empty}")
+	@Size(min = 3, max = 20, message = "{username_invalid_size}")
 	private String username;
 
-	@NotBlank
+	@NotBlank(message = "{password_invalid_empty}")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}|;:'\",.<>?/`~]).{8,32}$", message = "{password_invalid_format}")
 	private String password;
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
